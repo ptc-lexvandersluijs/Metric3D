7762548820d6c7b871e45a63cd85581de6ef6d60
Lex van der Sluijs <lexvandersluijs@gmail.com>
2025-02-13 11:31:23 +0100

    Added new python scripts to process a folder structure. This script is based on the scripts named test_*.sh in the root of the project which only process a single folder.

---
+++ test_vit_giant2_innorunway_all.sh
@@ -0,0 +1,68 @@
+python mono/tools/test_scale_cano.py \
+    'mono/configs/HourglassDecoder/vit.raft5.giant2.py' \
+    --load-from ./weight/metric_depth_vit_giant2_800k.pth \
+    --test_data_path /media/lex/T9/datasets/test_object_image+depth_metric3d/ContinentalBrake-2024-04-23 \
+    --launcher None
+    
+python mono/tools/test_scale_cano.py \
+    'mono/configs/HourglassDecoder/vit.raft5.giant2.py' \
+    --load-from ./weight/metric_depth_vit_giant2_800k.pth \
+    --test_data_path /media/lex/T9/datasets/test_object_image+depth_metric3d/CrealityEnder3ProAssembly-2024-04-23 \
+    --launcher None
+    
+python mono/tools/test_scale_cano.py \
+    'mono/configs/HourglassDecoder/vit.raft5.giant2.py' \
+    --load-from ./weight/metric_depth_vit_giant2_800k.pth \
+    --test_data_path /media/lex/T9/datasets/test_object_image+depth_metric3d/MagnaCosma_Carmax-2024-04-23 \
+    --launcher None
+    
+python mono/tools/test_scale_cano.py \
+    'mono/configs/HourglassDecoder/vit.raft5.giant2.py' \
+    --load-from ./weight/metric_depth_vit_giant2_800k.pth \
+    --test_data_path /media/lex/T9/datasets/test_object_image+depth_metric3d/MagnaCosma_Formpol-2024-04-23 \
+    --launcher None
+    
+python mono/tools/test_scale_cano.py \
+    'mono/configs/HourglassDecoder/vit.raft5.giant2.py' \
+    --load-from ./weight/metric_depth_vit_giant2_800k.pth \
+    --test_data_path /media/lex/T9/datasets/test_object_image+depth_metric3d/MagnaCosma_Montezuma-2024-04-23 \
+    --launcher None    
+
+python mono/tools/test_scale_cano.py \
+    'mono/configs/HourglassDecoder/vit.raft5.giant2.py' \
+    --load-from ./weight/metric_depth_vit_giant2_800k.pth \
+    --test_data_path /media/lex/T9/datasets/test_object_image+depth_metric3d/MagnaCosma_Venest-2024-04-23 \
+    --launcher None  
+    
+    
+python mono/tools/test_scale_cano.py \
+    'mono/configs/HourglassDecoder/vit.raft5.giant2.py' \
+    --load-from ./weight/metric_depth_vit_giant2_800k.pth \
+    --test_data_path /media/lex/T9/datasets/test_object_image+depth_metric3d/MultipleObjects-2024-04-23 \
+    --launcher None  
+    
+python mono/tools/test_scale_cano.py \
+    'mono/configs/HourglassDecoder/vit.raft5.giant2.py' \
+    --load-from ./weight/metric_depth_vit_giant2_800k.pth \
+    --test_data_path /media/lex/T9/datasets/test_object_image+depth_metric3d/Nespresso-2024-04-23 \
+    --launcher None  
+    
+python mono/tools/test_scale_cano.py \
+    'mono/configs/HourglassDecoder/vit.raft5.giant2.py' \
+    --load-from ./weight/metric_depth_vit_giant2_800k.pth \
+    --test_data_path /media/lex/T9/datasets/test_object_image+depth_metric3d/PorterCableCircularSaw-2024-04-23 \
+    --launcher None 
+    
+python mono/tools/test_scale_cano.py \
+    'mono/configs/HourglassDecoder/vit.raft5.giant2.py' \
+    --load-from ./weight/metric_depth_vit_giant2_800k.pth \
+    --test_data_path /media/lex/T9/datasets/test_object_image+depth_metric3d/ValidationTestbed-2024-04-23 \
+    --launcher None 
+    
+python mono/tools/test_scale_cano.py \
+    'mono/configs/HourglassDecoder/vit.raft5.giant2.py' \
+    --load-from ./weight/metric_depth_vit_giant2_800k.pth \
+    --test_data_path /media/lex/T9/datasets/test_object_image+depth_metric3d/VSYPump-2024-04-23 \
+    --launcher None   
+    
+    
